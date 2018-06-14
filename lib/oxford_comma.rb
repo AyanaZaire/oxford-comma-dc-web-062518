0a4bd02fbdc597d8da49051c56a980b278e9dee4
AyanaZaire <ayanazaire@gmail.com>
2018-06-14 18:33:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.length == 3
-    return "#{array[2]} and #{array[3]}"
-  elsif 3 < array.length
-    array[-2].insert(1, "and ")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
   end
   array.join(", ")    
 end
